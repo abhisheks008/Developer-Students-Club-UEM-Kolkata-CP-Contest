f6bb0f50dfe1e33918bc6b2cf7713a3c147c19d0
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2020-11-08 10:58:50 +0530

    Create Extra long factorials.py

---
+++ Extra long factorials.py
@@ -0,0 +1,7 @@
+# Extra long factorials of DSC UEM Kolkata Competitive Programming Contest
+# Solution in Python Programming Language
+# Author Abhishek Sharma
+# Date : 08.11.2020
+
+from math import factorial as f
+print (f(int(input())))
